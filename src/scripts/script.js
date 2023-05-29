0de46bbf1a707cb034be4b259a7e53ccb2470aa4
Luis <lgmarchioreh@gmail.com>
2023-05-29 08:30:14 -0300

    projeto

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,16 @@
 /* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function showModal(){
+    const button = document.querySelector(".btn-cadastrar");
+    const modalContainer = document.querySelector("#modalContainer");
+    const closeButton = document.querySelector(".closeMo");
+
+    button.addEventListener("click", () => {
+        modalContainer.showModal();
+    })
+
+    closeButton.addEventListener("click", () => {
+        modalContainer.close();
+    });
+}
+
+showModal();
